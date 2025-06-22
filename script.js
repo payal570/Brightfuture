02c930baa48b58691b0491b3b54c1a7ab7aec17b
payal570 <payal57076@gmail.com>
2025-06-22 12:40:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -31,12 +31,29 @@ function pageScrollFunction(){
     }
 }
 //notes popup
-function toggle(){
-    var blur = document.getElementById('blur');
-    blur.classList.toggle('active')
-    var popup = document.getElementById('popup');
-    popup.classList.toggle('active')
-}
+var modalBtns = document.querySelectorAll('.modal-open')
+
+modalBtns.forEach(function(btn){
+    btn.onclick = function() {
+        var modal = btn.getAttribute("data-modal");
+
+        document.getElementById(modal).style.display = "block";
+    };
+});
+
+var closeBtns = document.querySelectorAll('.modal-close');
+
+closeBtns.forEach(function(btn){
+    btn.onclick = function(){
+        var modal = (btn.closest(".modal").style.display = "none");
+    };
+});
+
+window.onclick = function(e){
+    if(e.target.className === "modal"){
+        e.target.style.display = "none";
+    }
+};
 
 // reference to buttons
 var btnLeft = document.getElementById('prev_btn');
